d90fe6691ace13a2f6d448a1c95b30168db06255
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2024-12-10 10:07:58 +0000

    自前のデザインに統一化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
-<template>
-  <h1>Hello World</h1>
-</template>
-
 <script setup>
+  import { ref } from "vue";
+  import TabItem from "./components/TabItem.vue";
+
+  import Default from "./components/window/default/00-main.vue";
+  import License from "./components/window/license/00-main.vue";
+
+  const currentTabId = ref("default");
+  const tabData = {
+    "default" : {
+      text: "メイン",
+      cmp: Default,
+    },
+    "license" : {
+      text: "ライセンス",
+      cmp: License,
+    }
+  };
+
+  function onClickTabButton(id){
+    if(currentTabId.value != id) {
+      currentTabId.value = id;
+    }
+  }
 </script>
 
+<template>
+  <header>
+    <ul id="tab">
+      <TabItem v-for="tabId in Object.keys(tabData)" :key="tabId" :tab-id="tabId" :text="tabData[tabId].text" :is-clicked="tabId == currentTabId" @onclick="(id) => {onClickTabButton(id)}" />
+    </ul>
+    <hr>
+  </header>
+
+  <main>
+    <div v-for="key in Object.keys(tabData)" :key="key" :class="currentTabId == key ? 'selected-window' : 'unselected-window'">
+      <component :is="tabData[key].cmp" />
+    </div>
+  </main>
+</template>
+
 <style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #tab {
+    display: inline-block;
+    padding-left:0;
+    list-style: none;
+    margin-block: 0;
+    place-items: left;
+    background-color: #777;
+    width: 100%;
+  }
+  hr {
+    margin: 0;
+  }
+  .unselected-window{
+    display: none;
+  }
 </style>
---
+++ src/components/TabItem.vue
@@ -0,0 +1,36 @@
+<script setup>
+  import { defineEmits, defineProps } from 'vue';
+  
+  const props = defineProps(["tabId", "text", "isClicked"]);
+  const $emit = defineEmits(["onclick"]);
+
+  function clicked(){
+    $emit("onclick", props.tabId);
+  }
+</script>
+
+<template>
+  <li>
+    <button v-on:click="clicked" :class="isClicked ? 'selected-btn' : 'unselected-btn'">{{ text }}</button>
+  </li>
+</template>
+
+<style scoped>
+  li {
+    display: inline-block;
+  }
+  button{
+    color: #fff;
+    background-color: #333;
+    width: 7.5rem;
+  }
+  button:hover{
+    background-color: #555;
+  }
+  .selected-btn{
+    background-color: #555;
+  }
+  .selected-btn:hover{
+    background-color: #777;
+  }
+</style>
